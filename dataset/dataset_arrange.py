c2793349c340832bf4e5ae6a71098052b1e089e1
Mohammad Faiz Abid <121609515+atfaizzz@users.noreply.github.com>
2026-02-11 04:07:23 +0530

    T1: Project Setup, Dataset Cleaning

---
+++ dataset/dataset_arrange.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import ast
+
+df = pd.read_csv('all_job_post.csv')
+
+def clean_skills(skills):
+    if pd.isna(skills):
+        return ""
+    if isinstance(skills, str):
+        skills = ast.literal_eval(skills)  # "['a','b']" → ['a','b']
+    return " ".join(skills)
+
+df['skills'] = df['job_skill_set'].apply(clean_skills)
+df['job_role'] = df['job_title']
+
+df_new = df[['skills', 'job_role']]
+df_new.to_csv('skills_job_role.csv', index=False)
